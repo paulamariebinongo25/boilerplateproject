459ec622d740111e96d74cc10641cd92f7815065
paulangcrackers <bmariepaula121@gmail.com>
2020-01-15 15:14:30 +0800

    Add the body style in global styles

--- app/global-styles.ts
+++ app/global-styles.ts
@@ -3,9 +3,9 @@ import { createGlobalStyle } from 'styles/styled-components';
 const GlobalStyle = createGlobalStyle`
   html,
   body {
-    /* height: 100%;
+    height: 100%;
     width: 100%;
-    line-height: 1.5; */
+    line-height: 1.5;
   }
 
   body {
